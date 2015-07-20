f2b185abbca5525c03a41b93289eb9b4aee569b0
Vladimir Magamedov <vladimir@magamedov.com>
2015-07-20 21:44:34 +0300

    Added setup.py script and tox.ini file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Kinko',
+    version='0.1.dev0',
+    description='Experimental frontend DSL',
+    author='Vladimir Magamedov',
+    author_email='vladimir@magamedov.com',
+    url='https://github.com/vmagamedov/kinko',
+    packages=['kinko'],
+    license='BSD',
+)
