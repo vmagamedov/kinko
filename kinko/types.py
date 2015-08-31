1fb3b839d4f548e420bf59c5ae809bf8c7445877
Vladimir Magamedov <vladimir@magamedov.com>
2015-08-31 20:09:21 +0300

    Refactored type classes.

--- kinko/types.py
+++ kinko/types.py
@@ -29,23 +29,23 @@ class OutputType(with_metaclass(_type_base(), object)):
 
 class TypingMeta(TypingMetaBase):
 
-    def __init__(cls, *args, **kwargs):
-        pass
+    def __cls_init__(cls, *args):
+        raise NotImplementedError
 
     def __getitem__(cls, parameters):
-        return cls.__class__(cls.__name__, cls.__bases__, dict(cls.__dict__),
-                             parameters)
+        type_ = cls.__class__(cls.__name__, cls.__bases__, dict(cls.__dict__))
+        type_.__cls_init__(parameters)
+        return type_
 
 
 class TypeVarMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, instance=None):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
+    def __cls_init__(cls, instance):
         cls.__instance__ = instance
-        return cls
 
     def __repr__(cls):
         return '{}[{!r}]'.format(cls.__name__, cls.__instance__)
+
 
 class TypeVar(with_metaclass(TypeVarMeta, object)):
     pass
@@ -53,14 +53,13 @@ class TypeVar(with_metaclass(TypeVarMeta, object)):
 
 class FuncMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, parameters=(None, None)):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
-        cls.__args__, cls.__result__ = parameters
-        return cls
+    def __cls_init__(cls, params):
+        cls.__args__, cls.__result__ = params
 
     def __repr__(cls):
         return '{}[{!r}, {!r}]'.format(cls.__name__, cls.__args__,
                                        cls.__result__)
+
 
 class Func(with_metaclass(FuncMeta, object)):
     pass
@@ -68,13 +67,12 @@ class Func(with_metaclass(FuncMeta, object)):
 
 class VarArgsMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, arg_type=None):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
+    def __cls_init__(cls, arg_type):
         cls.__arg_type__ = arg_type
-        return cls
 
     def __repr__(cls):
         return '{}[{!r}]'.format(cls.__name__, cls.__arg_type__)
+
 
 class VarArgs(with_metaclass(VarArgsMeta, object)):
     pass
@@ -82,14 +80,13 @@ class VarArgs(with_metaclass(VarArgsMeta, object)):
 
 class NamedArgMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, parameters=(None, None)):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
-        cls.__arg_name__, cls.__arg_type__ = parameters
-        return cls
+    def __cls_init__(cls, params):
+        cls.__arg_name__, cls.__arg_type__ = params
 
     def __repr__(cls):
         return '{}[{}={!r}]'.format(cls.__name__, cls.__arg_name__,
                                     cls.__arg_type__)
+
 
 class NamedArg(with_metaclass(NamedArgMeta, object)):
     pass
@@ -97,13 +94,12 @@ class NamedArg(with_metaclass(NamedArgMeta, object)):
 
 class QuotedMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, arg_type=None):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
+    def __cls_init__(cls, arg_type):
         cls.__arg_type__ = arg_type
-        return cls
 
     def __repr__(cls):
         return '{}[{!r}]'.format(cls.__name__, cls.__arg_type__)
+
 
 class Quoted(with_metaclass(QuotedMeta, object)):
     pass
@@ -111,13 +107,12 @@ class Quoted(with_metaclass(QuotedMeta, object)):
 
 class ListTypeMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, item_type=None):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
+    def __cls_init__(cls, item_type):
         cls.__item_type__ = item_type
-        return cls
 
     def __repr__(cls):
         return '{}[{!r}]'.format(cls.__name__, cls.__item_type__)
+
 
 class ListType(with_metaclass(ListTypeMeta, object)):
     pass
@@ -125,14 +120,13 @@ class ListType(with_metaclass(ListTypeMeta, object)):
 
 class DictTypeMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, parameters=(None, None)):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
-        cls.__key_type__, cls.__value_type__ = parameters
-        return cls
+    def __cls_init__(cls, params):
+        cls.__key_type__, cls.__value_type__ = params
 
     def __repr__(cls):
         return '{}[{!r}={!r}]'.format(cls.__name__, cls.__key_type__,
                                       cls.__value_type__)
+
 
 class DictType(with_metaclass(DictTypeMeta, object)):
     pass
@@ -140,13 +134,12 @@ class DictType(with_metaclass(DictTypeMeta, object)):
 
 class RecordTypeMeta(TypingMeta):
 
-    def __new__(typ, name, bases, namespace, parameters=None):
-        cls = TypingMeta.__new__(typ, name, bases, namespace)
-        cls.__items__ = parameters or {}
-        return cls
+    def __cls_init__(cls, items):
+        cls.__items__ = items.copy()
 
     def __repr__(cls):
         return '{}[{!r}]'.format(cls.__name__, cls.__items__)
+
 
 class RecordType(with_metaclass(RecordTypeMeta, object)):
     pass
