8e0eea8d2d03a6dfb84e8525c651d7ef9a5ab6de
Vladimir Magamedov <vladimir@magamedov.com>
2016-06-27 15:25:23 +0300

    Refactored test_sugar module

--- tests/test_sugar.py
+++ tests/test_sugar.py
@@ -1,150 +1,161 @@
 from kinko.nodes import Tuple, Symbol, List, String
 from kinko.sugar import InterpolateString
 
-from .base import TestCase, NODE_EQ_PATCHER
+from .base import NODE_EQ_PATCHER
 from .test_parser import parse, parse_raw
 
 
-class TestInterpolateString(TestCase):
-    ctx = [NODE_EQ_PATCHER]
+def interpolate(node):
+    return InterpolateString().visit(node)
 
-    def interpolate(self, node):
-        return InterpolateString().visit(node)
 
-    def checkLocation(self, src, node, fragment):
-        a, b = node.location.start.offset, node.location.end.offset
-        assert src[a:b] == fragment
+def check_eq(first, second):
+    with NODE_EQ_PATCHER:
+        assert first == second
 
-    def testInterpolate(self):
-        src = 'foo "before {a} {b.c} after"'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
-                Tuple([Symbol('join'), List([
-                    String('before '),
-                    Symbol('a'),
-                    String(' '),
-                    Symbol('b.c'),
-                    String(' after'),
-                ])]),
+
+def check_location(src, node, fragment):
+    a, b = node.location.start.offset, node.location.end.offset
+    assert src[a:b] == fragment
+
+
+def test_interpolate():
+    src = 'foo "before {a} {b.c} after"'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            Tuple([Symbol('join'), List([
+                String('before '),
+                Symbol('a'),
+                String(' '),
+                Symbol('b.c'),
+                String(' after'),
             ])]),
-        )
-        items = node.values[0].values[1].values[1].values
-        self.checkLocation(src, items[0], '"before ')
-        self.checkLocation(src, items[1], 'a')
-        self.checkLocation(src, items[2], ' ')
-        self.checkLocation(src, items[3], 'b.c')
-        self.checkLocation(src, items[4], ' after"')
+        ])]),
+    )
+    items = node.values[0].values[1].values[1].values
+    check_location(src, items[0], '"before ')
+    check_location(src, items[1], 'a')
+    check_location(src, items[2], ' ')
+    check_location(src, items[3], 'b.c')
+    check_location(src, items[4], ' after"')
 
-    def testInterpolateFirst(self):
-        src = 'foo "{a} after"'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
-                Tuple([Symbol('join'), List([
-                    Symbol('a'),
-                    String(' after'),
-                ])]),
+
+def test_interpolate_first():
+    src = 'foo "{a} after"'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            Tuple([Symbol('join'), List([
+                Symbol('a'),
+                String(' after'),
             ])]),
-        )
-        items = node.values[0].values[1].values[1].values
-        self.checkLocation(src, items[0], 'a')
-        self.checkLocation(src, items[1], ' after"')
+        ])]),
+    )
+    items = node.values[0].values[1].values[1].values
+    check_location(src, items[0], 'a')
+    check_location(src, items[1], ' after"')
 
-    def testInterpolateLast(self):
-        src = 'foo "before {a}"'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
-                Tuple([Symbol('join'), List([
-                    String('before '),
-                    Symbol('a'),
-                ])]),
-            ])]),
-        )
-        items = node.values[0].values[1].values[1].values
-        self.checkLocation(src, items[0], '"before ')
-        self.checkLocation(src, items[1], 'a')
 
-    def testInterpolateVarOnly(self):
-        src = 'foo "{a}"'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
+def test_interpolate_last():
+    src = 'foo "before {a}"'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            Tuple([Symbol('join'), List([
+                String('before '),
                 Symbol('a'),
             ])]),
-        )
-        self.checkLocation(src, node.values[0].values[1], 'a')
+        ])]),
+    )
+    items = node.values[0].values[1].values[1].values
+    check_location(src, items[0], '"before ')
+    check_location(src, items[1], 'a')
 
-    def testInterpolateStringOnly(self):
-        src = 'foo "text"'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
-                String('text'),
-            ])]),
-        )
-        self.checkLocation(src, node.values[0].values[1], '"text"')
 
-    def testInterpolateEmptyString(self):
-        src = 'foo ""'
-        node = self.interpolate(parse_raw(src))
-        self.assertEqual(
-            node,
-            List([Tuple([
-                Symbol('foo'),
-                String(''),
-            ])]),
-        )
-        self.checkLocation(src, node.values[0].values[1], '""')
+def test_interpolate_var_only():
+    src = 'foo "{a}"'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            Symbol('a'),
+        ])]),
+    )
+    check_location(src, node.values[0].values[1], 'a')
 
-    def testInterpolateInvalid(self):
-        self.assertEqual(
-            self.interpolate(parse_raw('foo "before {.a} {b.} {a..b} {.} '
-                                       '{..} after"')),
-            List([Tuple([
-                Symbol('foo'),
-                String('before {.a} {b.} {a..b} {.} {..} after'),
+
+def test_interpolate_string_only():
+    src = 'foo "text"'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            String('text'),
+        ])]),
+    )
+    check_location(src, node.values[0].values[1], '"text"')
+
+
+def test_interpolate_empty_string():
+    src = 'foo ""'
+    node = interpolate(parse_raw(src))
+    check_eq(
+        node,
+        List([Tuple([
+            Symbol('foo'),
+            String(''),
+        ])]),
+    )
+    check_location(src, node.values[0].values[1], '""')
+
+
+def test_interpolate_invalid():
+    check_eq(
+        interpolate(parse_raw('foo "before {.a} {b.} {a..b} {.} '
+                              '{..} after"')),
+        List([Tuple([
+            Symbol('foo'),
+            String('before {.a} {b.} {a..b} {.} {..} after'),
+        ])]),
+    )
+    check_eq(
+        interpolate(parse_raw('foo "before {.a} {b.} {c} {a..b} '
+                              '{.} {..} after"')),
+        List([Tuple([
+            Symbol('foo'),
+            Tuple([Symbol('join'), List([
+                String('before {.a} {b.} '),
+                Symbol('c'),
+                String(' {a..b} {.} {..} after'),
             ])]),
-        )
-        self.assertEqual(
-            self.interpolate(parse_raw('foo "before {.a} {b.} {c} {a..b} '
-                                       '{.} {..} after"')),
-            List([Tuple([
+        ])]),
+    )
+
+
+def test_parser():
+    node = parse("""
+    foo
+      "bar {value} baz"
+    """)
+    check_eq(
+        node,
+        List([
+            Tuple([
                 Symbol('foo'),
                 Tuple([Symbol('join'), List([
-                    String('before {.a} {b.} '),
-                    Symbol('c'),
-                    String(' {a..b} {.} {..} after'),
+                    String('bar '),
+                    Symbol('value'),
+                    String(' baz'),
                 ])]),
-            ])]),
-        )
-
-    def testParser(self):
-        node = parse("""
-        foo
-          "bar {value} baz"
-        """)
-        self.assertEqual(
-            node,
-            List([
-                Tuple([
-                    Symbol('foo'),
-                    Tuple([Symbol('join'), List([
-                        String('bar '),
-                        Symbol('value'),
-                        String(' baz'),
-                    ])]),
-                ]),
             ]),
-        )
+        ]),
+    )
